0b7dd210a49c860e87edfe31b2395d3be2f85f4a
HyeonSeong Jin <wlsgustjd88@naver.com>
2023-06-05 17:37:40 +0900

    1

--- DevProject/src/main/java/kr/or/ddit/controller/noticeBoard/service/INoticeService.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeBoard/service/INoticeService.java
@@ -1,10 +1,16 @@
 package kr.or.ddit.controller.noticeBoard.service;
 
+import java.util.List;
+
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PaginationInfoVO;
 
 public interface INoticeService {
 	public ServiceResult insertNotice(NoticeVO noticeVO);
 	public NoticeVO selectNotice(int boNo);
 	public ServiceResult updateNotice(NoticeVO noticeVO);
+	public ServiceResult deleteNotice(int boNo);
+	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
+	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
 }
